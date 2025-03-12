9a1956102cedfe262e5718a1e5b81a83334d1c44
Andrew Roan <roanutil@users.noreply.github.com>
2025-03-12 09:51:02 -0500

    Remove copyright lines from source header per PR feedback
    feature/formatting-ci-and-linting

--- Sources/Base64.swift
+++ Sources/Base64.swift
@@ -1,11 +1,6 @@
 // Base64.swift
 // SwiftyBeaver
 //
-// Copyright (c) 2015 Sebastian Kreutzberger
-// All rights reserved.
-//
-// Copyright 2025 MFB Technologies, Inc.
-//
 // This source code is licensed under the MIT License (MIT) found in the
 // LICENSE file in the root directory of this source tree.
 
